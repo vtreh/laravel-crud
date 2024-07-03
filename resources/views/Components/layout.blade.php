f770778433f13d145820e936fec7204b54986cfd
Vitalii Trehubov <tregubovvakharkov@gmail.com>
2024-07-03 14:14:19 +0300

    configure vite for asset bundling

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 
 <body class="h-full">
